b709b28cbaf9496839343626c9650f2e7c59d64b
Luis Eckert <s211288@student.dhbw-mannheim.de>
2023-12-01 02:45:53 +0100

    restapi fixed login auth allways false due double hashing

--- Project/blokusserver/authentication/authentication/serializers.py
+++ Project/blokusserver/authentication/authentication/serializers.py
@@ -1,7 +1,18 @@
 # authentication/serializers.py
+import logging
+from sys import stdout
 from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 from .models import CustomUser
+
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+
+logging.basicConfig(handlers=[logging.StreamHandler(stdout)],
+                    level = logging.DEBUG,
+                    format= LOG_FORMAT,
+                    datefmt='%d/%m/%Y %H:%M:%S')
+
+logger:logging.Logger = logging.getLogger()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,5 +21,7 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
+        logger.info({validated_data['password']})
         validated_data['password'] = make_password(validated_data['password'])
+        logger.info({validated_data['password']})
         return super(UserSerializer, self).create(validated_data)
--- Project/blokusserver/authentication/authentication/views.py
+++ Project/blokusserver/authentication/authentication/views.py
@@ -1,4 +1,7 @@
+import logging
+from sys import stdout
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import check_password
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,6 +10,16 @@ from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
 from .models import CustomUser
 from .serializers import UserSerializer
+
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+
+logging.basicConfig(handlers=[logging.StreamHandler(stdout)],
+                    level = logging.DEBUG,
+                    format= LOG_FORMAT,
+                    datefmt='%d/%m/%Y %H:%M:%S')
+
+logger:logging.Logger = logging.getLogger()
+
 
 class UserRegistrationView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
@@ -21,7 +34,7 @@ class UserRegistrationView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         #Hashing the password before saving the user
-        serializer.save(password=make_password(serializer.validated_data['password']))
+        serializer.save(password=serializer.validated_data['password'])
 
 
 class UserLoginView(APIView):
@@ -30,8 +43,14 @@ class UserLoginView(APIView):
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
+        
 
         user = CustomUser.objects.filter(username=username).first()
+
+
+        logger.info({})
+        logger.info({user.password})
+
 
         if user and user.check_password(password):
             token, created = Token.objects.get_or_create(user=user)
