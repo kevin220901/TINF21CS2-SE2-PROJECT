32130e4b10b88562028a3d1751722150033430a0
Kevin Wagner <kwagner@k-wagner.me>
2023-12-31 04:18:50 +0100

    Add Failed to load image

--- Project/client/how_to_play.py
+++ Project/client/how_to_play.py
@@ -17,9 +17,15 @@ class HowToPlay:
         layout = QGridLayout()
         popup.setModal(True)
         image_label = QLabel()
-        pixmap = QPixmap("./assets/how_to_play.png").scaled(800, 600, Qt.AspectRatioMode.KeepAspectRatio)
-        image_label.setPixmap(pixmap)
-        layout.addWidget(image_label)
-        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)  # Center alignment
+        pixmap = QPixmap("./assets/how_to_play.png")
+        if pixmap.isNull():
+            failed_label = QLabel("Failed to load image")
+            layout.addWidget(failed_label)
+            layout.setAlignment(Qt.AlignmentFlag.AlignCenter)  # Center alignment
+        else:
+            pixmap = pixmap.scaled(800, 600, Qt.AspectRatioMode.KeepAspectRatio)
+            image_label.setPixmap(pixmap)
+            layout.addWidget(image_label)
+            layout.setAlignment(Qt.AlignmentFlag.AlignCenter)  # Center alignment
         popup.setLayout(layout)
         popup.exec()
