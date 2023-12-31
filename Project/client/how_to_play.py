197eea6451bca205e53be103740b88dc9f7b8dac
Kevin Wagner <kwagner@k-wagner.me>
2023-12-31 01:32:39 +0100

    Change Volume

--- Project/client/how_to_play.py
+++ Project/client/how_to_play.py
@@ -10,11 +10,16 @@ class HowToPlay:
     def __init__(self, mainWindow):
         self.mainWindow = mainWindow
 
-    def h(self):
+    def how_to_playFrame(self):
         popup = QDialog()
         popup.setWindowTitle("How To Play")
-        popup.setFixedSize(800, 500)
+        popup.setFixedSize(600, 600)
         layout = QGridLayout()
         popup.setModal(True)
+        image_label = QLabel()
+        pixmap = QPixmap("./assets/how_to_play.png").scaled(800, 600, Qt.AspectRatioMode.KeepAspectRatio)
+        image_label.setPixmap(pixmap)
+        layout.addWidget(image_label)
+        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)  # Center alignment
+        popup.setLayout(layout)
         popup.exec()
-        
